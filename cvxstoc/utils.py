fce59808b6782f123f0d8d2353f10711ca0e04a9
Steven Diamond <stevend2@stanford.edu>
2015-09-07 18:16:15 -0700

    added special copy

---
+++ cvxstoc/utils.py
@@ -0,0 +1,41 @@
+from random_variable import RandomVariable
+from cvxpy.transforms.partial_optimize import PartialProblem
+from cvxpy import Problem
+import copy
+
+def replace_rand_vars(obj):
+    """Replaces all random variables with copies.
+
+    Parameters
+    ----------
+    obj : Object
+        The object to replace random variables in.
+
+    Returns
+    -------
+    Object
+        An object identical to obj, but with the random variables replaced.
+    """
+    if isinstance(obj, RandomVariable):
+        return copy.deepcopy(obj)
+    # Leaves outside of optimized variables are preserved.
+    elif len(obj.args) == 0:
+        return obj
+    elif isinstance(obj, PartialProblem):
+        prob = obj.args[0]
+        new_obj = replace_rand_vars(prob.objective)
+        new_constr = []
+        for constr in prob.constraints:
+            new_constr.append(
+                replace_rand_vars(constr)
+            )
+        new_args = [Problem(new_obj, new_constr)]
+        return obj.copy(new_args)
+    # Parent nodes are copied.
+    else:
+        new_args = []
+        for arg in obj.args:
+            new_args.append(
+                replace_rand_vars(arg)
+            )
+        return obj.copy(new_args)
