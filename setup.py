660de33919be16fe7789c7d8b4ba9dcaa6520526
Steven Diamond <stevend2@stanford.edu>
2016-01-22 10:08:54 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cvxstoc',
+    version='0.1',
+    author='Alnur Ali',
+    packages=['cvxstoc'],
+    license='GPLv3',
+    description='A domain-specific language for modeling stochastic convex optimization problems in Python.',
+    install_requires=["cvxpy >= 0.3.5",
+                      "pymc",
+                      "numpy >= 1.10",
+                      "scipy >= 0.16"],
+    use_2to3=True,
+)
