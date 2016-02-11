d4962ae3dee00184a88f68e2b2375ea4c8f9229c
Alnur Ali <alnurali@alnurMacBookPro.fios-router.home>
2016-02-10 22:39:35 -0500

    doc updates.

--- push_doc_changes_to_gh_pages.sh
+++ push_doc_changes_to_gh_pages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "This script presumes that you ran 'make html' from the sphinx folder, hit enter to continue."
+pause
 
 echo "Copying over latest doc files from the sphinx folder."
 cp -r ../sphinx/_build/html/* .
